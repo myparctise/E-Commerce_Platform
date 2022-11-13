0458096e1071e071cef0f0d6a3fe3c2659c1be0f
rishi0478 <rishimehra0478@gmail.com>
2022-11-13 19:24:51 +0530

    django admin customise

--- App/admin.py
+++ App/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import ProductItem,Product_details,AddToCart,Address,Orders,OrderList
 from .models import MyUser
 from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 admin.site.register(AddToCart)
@@ -13,8 +14,59 @@ class ProductItem_Admin(admin.ModelAdmin):
 @admin.register(Product_details)
 class Product_Admin(admin.ModelAdmin):
     list_display = ['product_name','product_title','slug','product_price','on_sale','sale_price']
+    search_fields = ['product_title__contains']
 
-admin.site.register(MyUser)
+
+# -------------------- User admin Customisation ---------------------
+class CartInline(admin.TabularInline):
+    model = AddToCart
+
+class OrderInline(admin.TabularInline):
+    model = OrderList
+
+class UserAdmin(UserAdmin):
+    model = MyUser
+    list_display = ['username','gender','mobile_no','address','is_staff','is_superuser']
+    list_filter = ('username','is_staff','is_active','is_superuser')
+    fieldsets = (
+        ("User Credentials", {
+            "fields": (
+                'username','password','gender','address','mobile_no'
+            ),
+        }),
+        
+        ("User Permissions",{'fields':('is_staff',('is_active','is_superuser'))}),
+        ("Important Dates",{"fields":('last_login','date_joined')}),
+        ("Advanced options",{
+            "classes":("collapse",),
+            'fields':('groups','user_permissions')
+        })
+
+    )
+    inlines = [
+        CartInline,OrderInline
+    ]
+    add_fieldsets = (
+        (
+            None,{
+                'classes':('wide'),
+                'fields':('username','password1','password2','is_staff',('is_active','is_superuser'))
+            }
+        ),
+    )
+
+    inlines = [
+        CartInline,OrderInline
+    ]
+    
+
+
+
+# admin.site.unregister(MyUser)
+admin.site.register(MyUser,UserAdmin)
+# -------------------- User admin Customisation end ---------------------
+
+
 # admin.site.register(Address)
 @admin.register(Address)
 class Addres(admin.ModelAdmin):
